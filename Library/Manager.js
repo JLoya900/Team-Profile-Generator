2cded3ed4324c68d26c9017cb23aae960751549a
Joaquin Loya <118794860+JLoya900@users.noreply.github.com>
2023-03-15 20:23:20 -0700

    Update Manager.js

--- Library/Manager.js
+++ Library/Manager.js
@@ -1,1 +1,22 @@
+const Employee = require('./Employee');
 
+class Manager extends Employee {
+    constructor({name, id, email, office}) {
+        
+        super({name, id, email});
+        this.office = office;
+        this.role = 'Manager';
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    getOffice() {
+        return this.office;
+    }
+
+}
+
+
+module.exports = Manager;
